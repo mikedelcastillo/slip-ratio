de4e3b5f6987c948d4521d29deaf1243dca31b71
mikedelcastillo <johnmichaeldc@gmail.com>
2023-08-05 16:53:26 +0000

    feat: test 2 dimensions

--- src/main.tsx
+++ src/main.tsx
@@ -32,18 +32,21 @@ type WheelUpdatePayload = {
   surfaceGrip: numberZeroToOne;
   contactPercent: numberZeroToOne;
   brakePressure: numberZeroToOne;
+  slipProfile: SurfaceSlipProfile,
+}
+
+type SurfaceSlipProfile = {
+  slipRatio: numberZeroToOne;
+  slope: number;
 }
 
 const limitValue = (value: number, limit: number) =>
   Math.max(-limit, Math.min(limit, value))
 
-// const computeSlipGripp = (value: number, slipRatio: number = 0.2, slope: number = 1) =>
-// value < slipRatio ? 1 : -slope * (value - slipRatio) + 1
+const compuleSlipProfile = (value: number, profile: SurfaceSlipProfile) =>
+  value < profile.slipRatio ? 1 : Math.max(0, -profile.slope * (value - profile.slipRatio) ** 2 + 1)
 
-const computeSlipGripp = (value: number, slipRatio: number = 0.1, coef: number = 1) =>
-  value < slipRatio ? 1 : -coef * (value - slipRatio) ** 2 + 1
-
-const getSlipAmount = (groundSpeed: number, wheelSurfaceSpeed: number) => {
+const getSlipRatio = (groundSpeed: number, wheelSurfaceSpeed: number) => {
   const wheel = Math.abs((wheelSurfaceSpeed - groundSpeed) / wheelSurfaceSpeed)
   const ground = Math.abs((groundSpeed - wheelSurfaceSpeed) / groundSpeed)
   if (isNaN(wheel) && isNaN(ground)) return 0
@@ -51,6 +54,17 @@ const getSlipAmount = (groundSpeed: number, wheelSurfaceSpeed: number) => {
   return Math.min(wheel, ground)
 }
 
+function areRadiansOverPI2Apart(radianA: number, radianB: number): boolean {
+  // Ensure radianA and radianB are within the range of 0 to 2 * PI
+  radianA = radianA % (2 * Math.PI);
+  radianB = radianB % (2 * Math.PI);
+
+  // Calculate the absolute difference between radianA and radianB
+  const absoluteDifference = Math.abs(radianA - radianB);
+
+  // Check if the absolute difference is greater than PI/2
+  return absoluteDifference >= Math.PI / 2;
+}
 const MAX_ACC = 0.1
 
 class Wheel {
@@ -66,25 +80,60 @@ class Wheel {
     this.config = config
   }
 
+  get groundSpeed() {
+    const { x, y } = this.velocity
+    return Math.sqrt(x * x + y * y)
+  }
+
+  get wheelGroundSpeed() {
+    const { x, y } = this.velocity
+    return x
+    const angleDiff = this.direction - this.velocityDirection
+    return Math.sqrt(x ** 2 + y ** 2 - 2 * x * y * Math.cos(angleDiff)) *
+      (areRadiansOverPI2Apart(this.direction, this.velocityDirection) ? 1 : -1)
+  }
+
+  get velocityDirection() {
+    const { x, y } = this.velocity
+    return Math.atan2(y, x)
+  }
+
+  get wheelSurfaceSpeed() {
+    return this.config.radius * this.angularVelocity
+  }
+
   update(payload: WheelUpdatePayload) {
     const { surfaceGrip, contactPercent, brakePressure } = payload;
     const { grip, radius, contactFriction, angularFriction } = this.config
-    const { x, y } = this.position
-    const { x: groundSpeed, y: vy } = this.velocity
-
     const maxGrip = grip * surfaceGrip * contactPercent
-    const wheelSurfaceSpeed = radius * this.angularVelocity
+    const { x: vx, y: vy } = this.velocity
 
-    const slipRatio = getSlipAmount(groundSpeed, wheelSurfaceSpeed)
+    const { wheelGroundSpeed, wheelSurfaceSpeed, velocityDirection} = this
 
-    const moveSpeed = limitValue((wheelSurfaceSpeed - this.velocity.x) * maxGrip, MAX_ACC * computeSlipGripp(slipRatio))
+    // const movingBackwards = 0
+
+    const slipRatio = getSlipRatio(wheelGroundSpeed, wheelSurfaceSpeed)
+
+    const acc = MAX_ACC * compuleSlipProfile(
+      slipRatio,
+      payload.slipProfile,
+    )
+    const moveSpeed = limitValue((wheelSurfaceSpeed - wheelGroundSpeed) * maxGrip, acc)
+    // console.log(slipRatio.toFixed(5), inlineGroundSpeed.toFixed(5), wheelSurfaceSpeed.toFixed(5), acc.toFixed(5), moveSpeed.toFixed(5))
+    console.log(wheelGroundSpeed.toFixed(5), this.velocity.x.toFixed(5),  wheelSurfaceSpeed.toFixed(5))
+
     this.velocity.x += moveSpeed
+    // this.velocity.y += moveSpeed * Math.sin(this.direction)
 
-    this.angularVelocity -= (moveSpeed / radius) * brakePressure
-    this.velocity.x *= 1 - ((1 - contactFriction) * maxGrip)
+    this.angularVelocity -= moveSpeed / radius
     this.angularVelocity *= (angularFriction - brakePressure * 0.5)
     this.rotationAngle += this.angularVelocity
+
+    const velFriction = 1 - ((1 - contactFriction) * maxGrip)
+    this.velocity.x *= velFriction
+    this.velocity.y *= velFriction
     this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
 
   }
 }
@@ -92,7 +141,7 @@ class Wheel {
 const config: WheelConfig = {
   radius: 10,
   width: 10,
-  grip: 1,
+  grip: 0.8,
   angularFriction: 0.975,
   contactFriction: 0.99,
   mass: 50
@@ -100,20 +149,26 @@ const config: WheelConfig = {
 
 const wheel = new Wheel(config)
 // wheel.direction = Math.PI / 4
-wheel.angularVelocity = 0.5
+// wheel.direction = Math.PI / 2
+// wheel.direction = Math.PI
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 wheel.position.x = canvas.width / 2
 wheel.position.y = canvas.height / 2
+wheel.angularVelocity = -2
 wheel.velocity.x = 2
 
 console.log(wheel)
 
 function loop() {
   wheel.update({
-    surfaceGrip: 0.1,
+    surfaceGrip: 0.9,
     contactPercent: 1,
-    brakePressure: 0.25,
+    brakePressure: 0,
+    slipProfile: {
+      slipRatio: 0.1,
+      slope: 0.6,
+    },
   })
 
   if (canvas === null || context === null) throw new Error("Canvas or context null")
@@ -132,7 +187,7 @@ function loop() {
 
     context.beginPath()
     context.moveTo(x, y)
-    const A = 0
+    const A = 10
     context.lineTo(x + wheel.velocity.x * A, y + wheel.velocity.y * A)
     context.stroke()
   }
